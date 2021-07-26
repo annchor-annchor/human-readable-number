4f60d59fb038190416d7deae582e7b6e37a60617
annchor.annchor <77202886+annchor-annchor@users.noreply.github.com>
2021-07-26 18:51:39 +0300

    feat: update main function in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 var numberToWords = require('number-to-words');
 
-module.exports = function toReadable (number) {
+function toReadable (number) {
   var result = numberToWords.toWords(number)
   result = result.replace('-', ' ');
   result = result.replace(',', '');
